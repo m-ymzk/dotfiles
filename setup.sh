82c78ae3ba27f862212ad92b6f6f6463dd8683be
m.yamazaki <m.yamazaki@fujitsu.com>
2023-01-08 13:33:00 +0900

    add vimrc(min) and molokai-colorscheme

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 mkdir -p ~/dotfiles/backup
 mv ~/.bash_aliases ~/dotfiles/backup/
 mv ~/.tmux.conf ~/dotfiles/backup/
+mv ~/.vim ~/dotfiles/backup/
+mv ~/.vimrc ~/dotfiles/backup/
 
 ln -s ~/dotfiles/.alias .bash_aliases
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/
+ln -s ~/dotfiles/.vimrc ~/
+ln -s ~/dotfiles/.vim ~/
